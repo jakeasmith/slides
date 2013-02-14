215f3240f1f34ffe9348d9924ab67d4b5622ef3d
Jake A. Smith <theman@jakeasmith.com>
2013-02-14 03:10:32 -0500

    First draft of git basic

---
+++ common/js/reveal.js
@@ -0,0 +1,19 @@
+Reveal.initialize({
+    // Full list of configuration options available here:
+    // https://github.com/hakimel/reveal.js#configuration
+    controls: false,
+    progress: true,
+    history: true,
+    rollingLinks: false,
+
+    // Optional libraries used to extend on reveal.js
+    dependencies: [
+        { src: '../vendor/reveal.js/lib/js/classList.js', condition: function() { return !document.body.classList; } },
+        { src: '../vendor/reveal.js/plugin/markdown/showdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        { src: '../vendor/reveal.js/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        { src: '../vendor/reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+        { src: '../vendor/reveal.js/plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } },
+        { src: '../vendor/reveal.js/plugin/remotes/remotes.js', async: true, condition: function() { return !!document.body.classList; } }
+    
+    ]
+});
