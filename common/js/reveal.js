fbaf0dd239750bb3ef0fa5183d35a239917f2de7
Jake A. Smith <theman@jakeasmith.com>
2013-03-27 00:15:52 -0400

    Added a cool function for pushing speaker notes to the console.

--- common/js/reveal.js
+++ common/js/reveal.js
@@ -17,3 +17,11 @@ Reveal.initialize({
     
     ]
 });
+
+Reveal.addEventListener( 'slidechanged', function( event ) {
+    // event.previousSlide, event.currentSlide, event.indexh, event.indexv
+    var notes = event.currentSlide.querySelector(".notes");
+    if(notes) {
+        console.info(notes.innerHTML.replace(/\n\s+/g,'\n'));
+    }
+} );
